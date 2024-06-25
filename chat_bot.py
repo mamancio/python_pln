87f00cc54fd0625b5cec2752a6ad6ea04424ea21
Matheus Mancio <mancio105@gmail.com>
2024-06-25 00:26:22 +0000

    correcao

--- chat_bot.py
+++ chat_bot.py
@@ -26,9 +26,8 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 if __name__ == "__main__":
   perguntas_respostas = carregar_perguntas("perguntas.txt")
   limiar_distancia = 10
-  while True:
-    pergunta = "Quem é você"
-    if pergunta == "sair":
-      break
-    resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-    print("Resposta:", resposta)
+ 
+  pergunta = "Quem é você"
+
+  resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
+  print("Resposta:", resposta)
