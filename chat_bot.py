72c579b6c67f392fd0b543a3c4b334bc5c3ed889
Matheus Mancio <mancio105@gmail.com>
2024-06-26 23:39:54 +0000

    qqqq

--- chat_bot.py
+++ chat_bot.py
@@ -23,7 +23,7 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 
 if __name__ == "__main__":
   perguntas_respostas = carregar_perguntas("perguntas.txt")
-  limiar_distancia = int(input("Digite o limiar de distância para considerar uma pergunta semelhante:"))
+  limiar_distancia = 10
   while True:
     pergunta = input("Faça uma pergunta:").lower()
     if pergunta == "sair":
